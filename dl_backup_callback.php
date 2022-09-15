1359105e78142c5b7e99f1f497218db1c1c54833
Weltolk <40228052+Weltolk@users.noreply.github.com>
2022-09-15 12:11:59 +0800

    Update dl_backup_callback.php

--- dl_backup_callback.php
+++ dl_backup_callback.php
@@ -1,11 +1,8 @@
 <?php if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); }
 function callback_init() {
-	$day = option::get('dl_backup_day');
-	$email = option::get('dl_backup_email');
-	$hour = option::get('dl_backup_hour');
-	if(empty($day)){option::set('dl_backup_day',1);}
-	if(empty($email)){option::set('dl_backup_email',EMAIL);}
-	if(empty($hour)){option::set('dl_backup_hour',10);}
+	option::set('dl_backup_day',1);
+	option::set('dl_backup_email',EMAIL);
+	option::set('dl_backup_hour',10);
 	cron::set('dl_backup','plugins/dl_backup/backup.php',0,0,0);
 }
 
@@ -15,5 +12,8 @@ function callback_inactive() {
 
 function callback_remove() {
 	global $m;
+	option::del('dl_backup_day');
+	option::del('dl_backup_email');
+	option::del('dl_backup_hour');
 	$m->query("DELETE FROM `".DB_NAME."`.`".DB_PREFIX."options` WHERE `name` LIKE '%dl_backup_%'");
 }
