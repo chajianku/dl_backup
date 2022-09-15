1454d945d6dffb582f571748059c30eb3b26a746
Weltolk <40228052+Weltolk@users.noreply.github.com>
2022-09-15 12:26:16 +0800

    Update dl_backup.php

--- dl_backup.php
+++ dl_backup.php
@@ -3,6 +3,8 @@ if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); }
 
 $day = option::get('dl_backup_day');
 if(empty($day)){option::set('dl_backup_day',1);}
+$hour = option::get('dl_backup_hour');
+if(empty($hour)){option::set('dl_backup_hour',10);}
 
 function dl_backup_navi() {
 	echo '<li ';
@@ -10,5 +12,6 @@ function dl_backup_navi() {
 	echo '><a href="index.php?mod=admin:setplug&plug=dl_backup"><span class="glyphicon glyphicon-cloud-upload"></span> 自动备份数据库</a></li>';
 }
 
-addAction('navi_2','dl_backup_navi');
+addAction('navi_1','dl_backup_navi');
+addAction('navi_7','dl_backup_navi');
 addAction('navi_3','dl_backup_navi');
